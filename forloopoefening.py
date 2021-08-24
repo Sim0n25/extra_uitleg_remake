dbdb400a1f263582241ff64650a2e4f56c57cce0
Simon <simon.fransen@student.kdg.be>
2021-08-24 19:14:48 +0200

    portfolio

---
+++ forloopoefening.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""info over project"""
+
+#import
+
+...
+
+__author__ = "Simon Fransen"
+__email__ = "simon.fransen@student.kdg.be"
+__status__ = "Production"
+
+for i in range(4):
+    print(i)
+
---
+++ try.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""info over project"""
+
+#import
+
+...
+
+__author__ = "Simon Fransen"
+__email__ = "simon.fransen@student.kdg.be"
+__status__ = "Production"
+
+
+try:
+  print(x)
+except:
+  print("exception")
