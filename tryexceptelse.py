cee13929325544a02c34dc4fcdafd529535e521a
Simon <simon.fransen@student.kdg.be>
2021-08-24 20:51:00 +0200

    portfolio

---
+++ tryexceptelse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""info over project"""
+
+#import
+
+...
+
+__author__ = "Simon Fransen"
+__email__ = "simon.fransen@student.kdg.be"
+__status__ = "Production"
+
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+
+  #try vindt geen error dus gaat het over naar de else
+
