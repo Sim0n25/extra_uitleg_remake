3c97789751982e2222efcb7de9fd320e520624db
Simon <simon.fransen@student.kdg.be>
2021-08-25 14:45:08 +0200

    portfolio

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""info over project"""
+
+#import
+
+...
+
+__author__ = "Simon Fransen"
+__email__ = "simon.fransen@student.kdg.be"
+__status__ = "Production"
+
+i = "hallo"
+for i in range(4):
+    print(i)
+
+
